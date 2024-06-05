567aa70b752e7bc8bc0b09e3e53dcf58f63d5182
Mouhamad Mourtada DIOP <mouhamadmourtadadiop@esp.sn>
2024-06-05 16:46:52 +0000

    shadcn ajouté

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hello world</p>
+
+      <Button onClick={() => setCount((count) => count + 1)}> count is: {count} </Button>
+
+    </>
+  )
+}
+
+export default App
